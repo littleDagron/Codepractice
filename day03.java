3462922883f996002e58ea4e1ac41b8b9a60c967
littleDagron <37390925+littleDagron@users.noreply.github.com>
2019-03-29 21:22:13 +0800

    day03

---
+++ day03.java
@@ -0,0 +1,21 @@
+public class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        // write your code here
+        int low = 0, high = nums.length-1, n = nums.length;
+        while(low < high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else high = mid;
+        }
+        return (high < n && nums[high] == target)? high : -1;
+    }
+}
